f43c495c12df368cf0e37a1e0a231e305e553394
João Vitor <joao.rubertcorrea@gmail.com>
2022-01-12 20:33:43 -0300

    arrays no javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+let array = [1,2,3,4,5,6,7,8,9,10];
+array.push(25);//adiciona um item ao array
+array.pop();//remove o ultimo item do array 
+array.shift();//remove o item do inicio do array 
+array.unshift(1);//adiciona um item ao inicio do array 
+
+
+console.log(array); //retorna todos numeros no array
+console.log(array.length); //retorna a quantidade de iteis no array
+console.log(array[0]); //retorna um item especifico do array 
+
